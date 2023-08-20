c92933aadde8b336dbf6737994bfce4186003d06
batteryhun <snuleex@gmail.com>
2023-08-21 04:57:33 +0900

    modify

--- js/background.js
+++ js/background.js
@@ -4,13 +4,13 @@ const chosenImage = bgImages[Math.floor(Math.random() * bgImages.length)];
 const bgImage = document.querySelector("#background__img");
 const bgBox = document.querySelector("#background__box");
 const instaBtn = document.querySelector(".icon-link__instagram");
-bgImage.src = `../img/${chosenImage}`;
+bgImage.src = `img/${chosenImage}`;
 
 refreshBtn.addEventListener("click", () => {
   console.log("colik");
   const chosenImage = bgImages[Math.floor(Math.random() * bgImages.length)];
   const bgImage = document.querySelector("#background__img");
-  bgImage.src = `../img/${chosenImage}`;
+  bgImage.src = `img/${chosenImage}`;
 });
 instaBtn.addEventListener("click", () => {
   bgBox.classList.toggle("hidden");
